6346b2908eb08e0cc114055eff4e287239739855
Pierre Fernique <pfernique@gmail.com>
2017-11-08 14:16:43 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,14 +1,12 @@
-export TEST_LEVEL=1
 conda config --set always_yes yes
-conda config --add channels r
 conda config --add channels statiskit
-if [[ ! "$ANACONDA_UPLOAD" = "statiskit" ]]; then
+if [[ ! "$ANACONDA_UPLOAD" = "openalea" ]]; then
   conda config --add channels $ANACONDA_UPLOAD
   if [[ ! "$ANACONDA_LABEL" = "main" ]]; then
       conda config --add channels $ANACONDA_UPLOAD/label/$ANACONDA_LABEL
   fi
 else
   if [[ ! "$ANACONDA_LABEL" = "main" ]]; then
-      conda config --add channels statiskit/label/$ANACONDA_LABEL
+      conda config --add channels openalea/label/$ANACONDA_LABEL
   fi
 fi
