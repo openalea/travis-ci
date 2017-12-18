c6e07fa51fbb1c9a7f456efa2a72ac270c0efd4e
Frederic Boudon <fredboudon@users.noreply.github.com>
2017-12-18 09:47:54 +0100

    add unstable for custom user build

--- config.sh
+++ config.sh
@@ -1,6 +1,7 @@
 conda config --set always_yes yes
 conda config --add channels openalea
 if [[ ! "$ANACONDA_UPLOAD" = "openalea" ]]; then
+  conda config --add channels openalea/label/unstable
   conda config --add channels $ANACONDA_UPLOAD
   if [[ ! "$ANACONDA_LABEL" = "main" ]]; then
       conda config --add channels $ANACONDA_UPLOAD/label/$ANACONDA_LABEL
