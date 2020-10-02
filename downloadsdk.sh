972f0ae6d04d8b782078f60537351d95539f7bb7
Frederic Boudon <fredboudon@users.noreply.github.com>
2020-10-02 14:22:48 +0200

    Update downloadsdk.sh

--- downloadsdk.sh
+++ downloadsdk.sh
@@ -1,6 +1,10 @@
 if [ `uname` = "Darwin" ];  then 
     wget -nv https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.9.sdk.tar.xz
     tar xzf MacOSX10.9.sdk.tar.xz
+    if [ -d MacOSX10.9.sdk ]; then
+        echo "Problem with download of the SDK"
+    fi
+    ls MacOSX10.9.sdk
     echo "CONDA_BUILD_SYSROOT:
  - `pwd`/MacOSX10.9.sdk
  " > conda/conda_build_config.yaml
