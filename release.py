106d667f2a795f2b1d6059c881352b4e60fcebc9
Frederic Boudon <fredboudon@users.noreply.github.com>
2021-03-18 15:14:43 +0100

    Update release.py
    Make more robust to request failure

--- release.py
+++ release.py
@@ -6,8 +6,12 @@ headers = {"Accept": "application/vnd.travis-ci.2+json"}
 url = "https://api.travis-ci.org/repos/" + os.environ["TRAVIS_REPO_SLUG"] + "/builds/" + os.environ["TRAVIS_BUILD_ID"]
 
 TRAVIS_JOB_NUMBER = int(os.environ["TRAVIS_JOB_NUMBER"][len(os.environ["TRAVIS_BUILD_NUMBER"]) + 1:])
+try:
+    status = requests.get(url, headers=headers).json()
+except:
+    status = None
 
-status = requests.get(url, headers=headers).json()
-for job in status['jobs'][:TRAVIS_JOB_NUMBER]:
-    if job["config"]["os"] == os.environ["TRAVIS_OS_NAME"] and not job["allow_failure"] and job["state"] in ['failed', 'errored']:
-        raise Exception("A previous job failed !")
+if status:
+    for job in status['jobs'][:TRAVIS_JOB_NUMBER]:
+        if job["config"]["os"] == os.environ["TRAVIS_OS_NAME"] and not job["allow_failure"] and job["state"] in ['failed', 'errored']:
+             raise Exception("A previous job failed !")
